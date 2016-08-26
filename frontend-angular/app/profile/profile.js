55b18c9b9e43c36d7a4e6724a5114c2e1044e728
hdelval <hdelval@debian>
2016-08-26 08:09:00 -0400

    add config server + templates

--- frontend-angular/app/profile/profile.js
+++ frontend-angular/app/profile/profile.js
@@ -13,10 +13,8 @@ angular.module('myApp.profile', ['ngRoute', 'ngCookies'])
 
 	/* ------ BEGIN INIT ------ */
 	$scope.request = {};
-	$scope.validatedChallenges = [];
 	$scope.nbChallengesAvailable = 0;
-	$scope.score = 0;
-	$scope.rank = 0;
+	$scope.totalScoreChallenges = 0;
 	$scope.nbUsers = 0;
 	$scope.user = $cookies.getObject('user') || {};
 	if(!$scope.user.nick){
@@ -29,31 +27,38 @@ angular.module('myApp.profile', ['ngRoute', 'ngCookies'])
 		$http.defaults.headers.common['X-CTF-AUTH'] = $scope.user.token;
 		$http.get('/v1.0/user/me').success(function(me){
 
+			$scope.user.score = 0;
+			$scope.user.rank = 0;
+			$scope.user.validatedChallenges = [];
 			// users scores -> rank + nbUsers
 			$http.get('/v1.0/user').success( function ( users ) {
 				$scope.nbUsers = users.length;
 				var scores = new Array($scope.nbUsers);
 				for(var userIt=0 ; userIt<$scope.nbUsers ; ++userIt){
 					scores[userIt] = 0;
-					$http.get('/v1.0/user/'+users[userIt].ID+'/validatedChallenges').success(function(validatedChalls){
-						for (var challIt=0; challIt < validatedChalls.length ; ++challIt){
-							$http.get('/v1.0/challenge/' + validatedChalls[challIt].ChallengeID).success(function(validatedChall){
-								scores[userIt] += validatedChall.points;
-								if(users[userIt].ID == me.ID){
-									// user score + validated challenges
-									$scope.validatedChallenges.push(validatedChall);
-									$scope.score += validatedChall.points;
-								}
-							}).error(function(error){
-								alert("An error occured : " + error.message);
-							});
+					$http.get('/v1.0/user/'+users[userIt].ID+'/validatedChallenges').success((function(userIterator){
+						return function(validatedChalls){
+							for (var challIt=0; challIt < validatedChalls.length ; ++challIt){
+								$http.get('/v1.0/challenge/' + validatedChalls[challIt].ChallengeID).success((function(userIter, currId) {
+								    return function(validatedChall) {
+										scores[userIter] += validatedChall.points;
+										if(me.ID == currId){
+											// user score + validated challenges
+											$scope.user.validatedChallenges.push(validatedChall);
+											$scope.user.score += validatedChall.points;
+										}
+								    }
+								})(userIterator, users[userIterator].ID)).error(function(error){
+									alert("An error occured : " + error.message);
+								});
+							}
 						}
-					}).error(function(error){
+					})(userIt)).error(function(error){
 						alert('An error occured :' + error.message);
 					});
 				}
 				scores.sort();
-				$scope.rank = scores.indexOf($scope.score) + 1;
+				$scope.user.rank = scores.indexOf($scope.user.score) + 1;
 			}).error(function(error){
 				alert('An error occured :' + error.message);
 			});
@@ -67,7 +72,14 @@ angular.module('myApp.profile', ['ngRoute', 'ngCookies'])
 			alert('You must login to access this page.');
 			$location.path("/user/login");
 		});
-
+		$http.get('/v1.0/challenge').success(function(challenges){
+			$scope.nbChallengesAvailable = challenges.length;
+			for(var challIt=0 ; challIt<challenges.length ; ++challIt){
+				$scope.totalScoreChallenges += challenges[challIt].points;
+			}
+		}).error(function(error){
+			alert('An error occured :' + error.message);
+		});
 	}
 	/* ------ END INIT ------ */
 
@@ -77,11 +89,22 @@ angular.module('myApp.profile', ['ngRoute', 'ngCookies'])
 		if($scope.request.password !== $scope.request.passwordConfirm){
 			alert("Password mismatch " )
 		}else{
-			$http.post('/v1.0/user/me/changePassword', $scope.request).success(function(data){
+			$http.put('/v1.0/user/me/changePassword', $scope.request).success(function(data){
 				$scope.response = data;
 			}).error(function(data){
 				alert("An error occured while processing request ");
 				$scope.response = data;
+			});
+		}
+	}
+	$scope.deleteAccount = function(){
+		var ask = confirm("Are you sure?");
+		if(ask){
+			$http.delete('/v1.0/user/me/unregister').success(function(response){
+				alert('Account deleted : ' + response.message);
+				$location.path("/");
+			}).error(function(error){
+				alert("An error occured while processing request : " + error.message);
 			});
 		}
 	}
--- frontend-angular/app/scoreboard/scoreboard.js
+++ frontend-angular/app/scoreboard/scoreboard.js
@@ -13,10 +13,30 @@ angular.module('myApp.scoreboard', ['ngRoute'])
 
 	/* ------ BEGIN INIT ------ */
 	$scope.request = {};
-	$http.get('/v1.0/user').success( function ( data ) {
-		$scope.users = data;
-	}).error(function(data){
-		alert("An error occured while processing request : " + data.message);
+	$http.get('/v1.0/user').success( function ( users ) {
+		$scope.users = users;
+		for(var userIt=0 ; userIt<users.length ; ++userIt){		
+			$scope.users[userIt].score = 0;
+			var currId = $scope.users[userIt].ID;
+			$http.get('/v1.0/user/'+currId+'/validatedChallenges').success((function(userIterator){
+				return function(validatedChalls){
+					for (var challIt=0; challIt < validatedChalls.length ; ++challIt){
+						$http.get('/v1.0/challenge/' + validatedChalls[challIt].ChallengeID).success((function(userIter){
+							return function(validatedChall){
+								$scope.users[userIter].score += validatedChall.points;
+							}
+						})(userIterator)).error(function(error){
+							alert("An error occured : " + error.message);
+						});
+					}
+				}
+			})(userIt)).error(function(error){
+				alert('An error occured :' + error.message);
+			});
+		}
+
+	}).error(function(error){
+		alert("An error occured while processing request : " + data.error);
 	});
 	/* ------ END INIT ------ */
 }]);
