851916f9f05cd17a9ed48824cfeee2c6ba426f2b
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:17:16 +0200

    add run script - to build and launch app

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /srv/ctf_go
+export GOPATH=`pwd`
+export PATH=$PATH:${GOROOT}/bin:${GOPATH}/bin
+sudo -u ctf_interne go build ctf/main
+echo "build done"
+sudo -u ctf_interne ./main
