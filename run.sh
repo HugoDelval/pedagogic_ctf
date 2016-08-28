a065e19f7ddcdffbfb83c293d5d03a6f626e0a0a
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:25:26 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 cd /srv/ctf_go
+su ctf_interne
 export GOPATH=`pwd`
 export PATH=$PATH:${GOROOT}/bin:${GOPATH}/bin
-sudo -u ctf_interne go get ctf/main
-sudo -u ctf_interne go build ctf/main
+go get ctf/main
+go build ctf/main
 echo "build done"
-sudo -u ctf_interne ./main
+./main
