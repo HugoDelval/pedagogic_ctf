dfada3820a41a03d2f87c7c69e2266d343fab788
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:03:15 +0200

    delete confirmation on rm (too long..) + fix typo

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 useradd ctf_interne
-rm -rfi /srv/ctf_go && mkdir /srv/ctf_go
+rm -rf /srv/ctf_go && mkdir /srv/ctf_go
 cp . -R /srv/ctf_go
-rm -rfi /srv/ctf_go && mkdir /srv/writable && chmod 733 /srv/writable
+rm -rf /srv/writable && mkdir /srv/writable && chmod 733 /srv/writable
 cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/ctf_go/src/ctf/utils/config.json
 chown ctf_interne /src/ctf_go -R
 chmod o-rwx /src/ctf_go -R
