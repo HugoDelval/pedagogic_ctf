2d8674dc4b399468a60444d66d51ed47af2574d8
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:09:48 +0200

    remove old users

--- init.sh
+++ init.sh
@@ -8,7 +8,12 @@ rm -rf /srv/writable && mkdir /srv/writable && chmod 733 /srv/writable -R
 cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/ctf_go/src/ctf/utils/config.json
 chown ctf_interne /srv/ctf_go -R
 chmod o-rwx /srv/ctf_go -R
+
+# Init challenges
 userdel injection_conf
 /srv/ctf_go/load_challenges.py injection_conf
+
+echo
 echo "Check src/ctf/utils/config.json !"
+
 
