7ed5ee763c916f8d1c12f3546ce40ff33b165263
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 20:56:41 +0200

    update readme install + 1st fix auto-install

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
 # CTF Pédagogique
 
 Ce repo permettra à ceux qui souhaitent apprendre la sécurité informatique de se lancer facilement.
+
+## Installation :
+run the script *init.sh*
+
+    git clone https://github.com/HugoDelval/ctf_pedagogique
+    cd ctf_pedagogique
+    sudo ./init.sh
+    
+    
--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ mkdir /srv/ctf_go
 cp . -R /srv/ctf_go
 mkdir /srv/writable
 chmod 733 /srv/writable
-/srv/cf_go/load_challenges.py injection_conf
-cp /srv/cf_go/src/ctf/utils/config.json.example /srv/cf_go/src/ctf/utils/config.json
+/srv/ctf_go/load_challenges.py injection_conf
+cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/cf_go/src/ctf/utils/config.json
 echo "Check src/ctf/utils/config.json !"
 
