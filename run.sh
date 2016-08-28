d9347ab0511cccdea52f471edfb97e368a09c2ec
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:36:05 +0200

    check if script launch with good user

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [ whoami != 'ctf_interne' ] ; then
+    echo "please launch using : "
+    echo "    sudo -u ctf_interne ./run.sh"
+    exit
+fi
+
 cd /srv/ctf_go
-su ctf_interne
 export GOPATH=`pwd`
 export PATH=$PATH:${GOROOT}/bin:${GOPATH}/bin
 go get ctf/main
