d264306f8f317a08e722faf3963823feb42b5a2d
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:06:55 +0200

    fix other typo

--- init.sh
+++ init.sh
@@ -3,10 +3,10 @@
 useradd ctf_interne
 rm -rf /srv/ctf_go && mkdir /srv/ctf_go
 cp . -R /srv/ctf_go
-rm -rf /srv/writable && mkdir /srv/writable && chmod 733 /srv/writable
+rm -rf /srv/writable && mkdir /srv/writable && chmod 733 /srv/writable -R
 cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/ctf_go/src/ctf/utils/config.json
-chown ctf_interne /src/ctf_go -R
-chmod o-rwx /src/ctf_go -R
+chown ctf_interne /srv/ctf_go -R
+chmod o-rwx /srv/ctf_go -R
 /srv/ctf_go/load_challenges.py injection_conf
 echo "Check src/ctf/utils/config.json !"
 
