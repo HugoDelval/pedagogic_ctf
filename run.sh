4254f773ccea7110f10e982d13101a846c9774cc
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 22:22:32 +0200

    run.sh check umask

--- run.sh
+++ run.sh
@@ -7,6 +7,19 @@ if [ ${user} != 'ctf_interne' ] ; then
     exit
 fi
 
+the_umask=`umask`
+if [ ${the_umask} != '0027' ] ; then
+    echo "Warning ! Your umask is : ${the_umask} "
+    echo "It should be 0027."
+    read -p "Are you sure to continue !? (y/n)" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Good choice !"
+        exit
+    fi
+fi
+
 cd /srv/ctf_go
 export GOPATH=`pwd`
 export PATH=$PATH:${GOROOT}/bin:${GOPATH}/bin
