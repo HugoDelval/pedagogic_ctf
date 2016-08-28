beeb0a65f095585fe32adfb55a5de6d8f66a6873
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-28 21:00:45 +0200

    changing auto install: delete automatically old installation

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 useradd ctf_interne
-mkdir /srv/ctf_go
+rm -rfi /srv/ctf_go && mkdir /srv/ctf_go
 cp . -R /srv/ctf_go
-mkdir /srv/writable
-chmod 733 /srv/writable
+rm -rfi /srv/ctf_go && mkdir /srv/writable && chmod 733 /srv/writable
+cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/ctf_go/src/ctf/utils/config.json
+chown ctf_interne /src/ctf_go -R
+chmod o-rwx /src/ctf_go -R
 /srv/ctf_go/load_challenges.py injection_conf
-cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/cf_go/src/ctf/utils/config.json
 echo "Check src/ctf/utils/config.json !"
 
