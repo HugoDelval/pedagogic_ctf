b339e3e3561917fe0fa11ad88bc29833b4581821
HugoDelval <hugo.delval@insa-lyon.fr>
2016-08-29 23:40:31 +0200

    starting design

--- init.sh
+++ init.sh
@@ -4,6 +4,12 @@ useradd ctf_interne
 mkdir /home/ctf_interne && chown ctf_interne:ctf_interne /home/ctf_interne -R
 chattr -i -R /srv/ctf_go/challs/
 rm -rf /srv/ctf_go && mkdir /srv/ctf_go
+
+export GOPATH=`pwd`
+export PATH=$PATH:${GOROOT}/bin:${GOPATH}/bin
+echo "Fetching golang requirements.."
+go get ctf/main
+
 cp . -R /srv/ctf_go
 rm -rf /srv/writable && mkdir /srv/writable && chmod 733 /srv/writable -R
 cp /srv/ctf_go/src/ctf/utils/config.json.example /srv/ctf_go/src/ctf/utils/config.json
