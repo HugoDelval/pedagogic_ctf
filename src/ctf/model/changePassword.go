6fa92ea73c5559938cb65d619d7d60dc57794347
HugoDelval <hugodelval@gmail.com>
2017-03-06 01:52:36 +0000

    fix 'too much useless imports'

--- src/ctf/model/changePassword.go
+++ src/ctf/model/changePassword.go
@@ -1,9 +1,5 @@
 package model
 
-import (
-	"github.com/jinzhu/gorm"
-)
-
 type ChangePassword struct {
 	Password            string                `json:"password"`
 	PasswordConfirm     string                `json:"passwordConfirm"`
