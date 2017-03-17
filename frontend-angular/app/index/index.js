080b00df1b95668cc4362bfeb683006e3621eb4a
Simon Vasseur <simon.vasseur@corp.ovh.com>
2017-03-17 16:54:12 +0000

    chore(front): modal instead of alert

--- frontend-angular/app/index/index.js
+++ frontend-angular/app/index/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.index', ['ngRoute', 'ui.ace'])
+angular.module('myApp.index', ['ngRoute', 'ui.ace', 'angularModalService'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {
@@ -9,7 +9,7 @@ angular.module('myApp.index', ['ngRoute', 'ui.ace'])
         });
     }])
 
-    .controller('IndexCtrl', ['$cookies', '$sce', '$scope', '$http', '$location', '$anchorScroll', function ($cookies, $sce, $scope, $http, $location, $anchorScroll) {
+    .controller('IndexCtrl', ['$cookies', '$sce', '$scope', '$http', '$location', '$anchorScroll', 'ModalService', function ($cookies, $sce, $scope, $http, $location, $anchorScroll, ModalService) {
 
         /* ------ BEGIN INIT ------ */
         $.fn.extend({
@@ -84,6 +84,8 @@ angular.module('myApp.index', ['ngRoute', 'ui.ace'])
         };
         $scope.execute = function (challengeId, path, extension) {
             $http.defaults.headers.common['X-CTF-AUTH'] = $scope.user.token;
+            var title = "";
+            var message = "";
             var req = {};
             if (path.indexOf("execute") !== -1) {
                 $http.post('/v1.0/challenge/' + challengeId + path, $scope.requestExecute[challengeId]).then(function (response) {
@@ -94,28 +96,33 @@ angular.module('myApp.index', ['ngRoute', 'ui.ace'])
                     })
                     $("#output_" + challengeId).delay(750).qcss({ backgroundColor: '#FFFF70' }).delay(750).qcss({ backgroundColor: 'white' }).delay(750).qcss({ backgroundColor: '#FFFF70' }).delay(750).qcss({ backgroundColor: 'white' }).delay(750);
                 }, function (response) {
-                    var error = response.data;
-                    alert(error.message);
+                    title = "Execution error";
+                    message = response.data.message;
+	            showModal(title, message);
                 });
             }
             else if (path.indexOf("validate") !== -1) {
                 // validate
                 $http.post('/v1.0/challenge/' + challengeId + path, $scope.requestValidate[challengeId]).then(function (response) {
-                    var data = response.data;
-                    alert(data.message); // modal
+                    title = "Success";
+                    message = response.data.message;
+	            showModal(title, message);
                 }, function (response) {
-                    var error = response.data;
-                    alert(error.message);
+                    title = "Validation error";
+                    message = response.data.message;
+	            showModal(title, message);
                 });
             } else if (path.indexOf("correct") !== -1) {
                 // correct
                 $scope.requestCorrect[challengeId][extension].language_extension = extension;
                 $http.post('/v1.0/challenge/' + challengeId + path, $scope.requestCorrect[challengeId][extension]).then(function (response) {
-                    var data = response.data;
-                    alert(data.message); // modal
+                    title = "Success";
+                    message = response.data.message;
+	            showModal(title, message);
                 }, function (response) {
-                    var error = response.data;
-                    alert(error.message);
+                    title = "Correction error";
+                    message = response.data.message;
+	            showModal(title, message);
                 });
             }
         };
@@ -141,5 +148,18 @@ angular.module('myApp.index', ['ngRoute', 'ui.ace'])
             $scope.language = language;
         }
         /* ------ END UTILS ------ */
+
+        function showModal(title, message) {
+	    ModalService.showModal({
+	      templateUrl: "partials/modal.html",
+	      controller: function() {
+	        this.title = title;
+	        this.message = message;
+	      },
+	      controllerAs : "modal"
+	    }).then(function(modal) {
+		modal.element.modal();
+	    });
+	}
 
     }]);
